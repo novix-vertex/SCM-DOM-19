465a1042f3b606b2033fa2d73b2fb6cead5723fa
chirag jain <jchirag0610@gmail.com>
2026-04-14 13:43:54 +0530

    white key notes added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ black_keys.forEach((black_key, index) => {
 
     black_key.style.width = ((window.innerWidth / 21) * 50 / 100) + "px";
     black_key.style.left = ((window.innerWidth / 21)) * index + ((window.innerWidth / 21) * 50 / 100) + (((window.innerWidth / 21) * 50 / 100) / 2) + "px";
-   
+
     switch (index) {
         case 2:
         case 6:
@@ -16,4 +16,53 @@ black_keys.forEach((black_key, index) => {
             break;
     };
 });
+
+
+//Sounds
+
+const notes = {
+    C: "assets/sounds/C4.mp3",
+    D: "assets/sounds/D4.mp3",
+    E: "assets/sounds/E4.mp3",
+    F: "assets/sounds/F4.mp3",
+    G: "assets/sounds/G4.mp3",
+    A: "assets/sounds/A4.mp3",
+    B: "assets/sounds/B4.mp3"
+};
+
+const octaves = {
+    3: 0.75, // low
+    4: 1,    // original
+    5: 1.25  // high
+};
+
+document.addEventListener("click", function (e) {
+    const key = e.target.closest(".key");
+    if (!key) return;
+
+    const note = key.dataset.note;
+    const octave = key.dataset.octave;
+
+    playNote(note, octave);
+});
+
+function playNote(note, octave) {
+    const audio = new Audio(notes[note]);
+    audio.playbackRate = octaves[octave];
+    audio.currentTime = 0;
+    audio.play();
+}
+
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/C4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/D4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/E4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/F4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/G4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/A4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/B4.mp3
+
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/Ds4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/Fs4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/Gs4.mp3
+// https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-mp3/As4.mp3
 
