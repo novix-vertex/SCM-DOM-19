d489a15981bdb1ecfb1a4dab30eae3b79cc68b20
chirag jain <jchirag0610@gmail.com>
2026-04-14 14:45:55 +0530

    black key placement corrected for last key which was taking extra space and producting horizontal scroll

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ black_keys.forEach((black_key, index) => {
 
     black_key.style.width = ((window.innerWidth / 21) * 50 / 100) + "px";
     black_key.style.left = ((window.innerWidth / 21)) * index + ((window.innerWidth / 21) * 50 / 100) + (((window.innerWidth / 21) * 50 / 100) / 2) + "px";
-
+    
+    if(index==20){
+        black_key.style.left = ((window.innerWidth / 21)) * index + ((window.innerWidth / 21) * 50 / 100) + "px";
+    }
     switch (index) {
         case 2:
         case 6:
